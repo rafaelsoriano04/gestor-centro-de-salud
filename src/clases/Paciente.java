b0db120417a8226036085032450503980ab64803
fredd <fredd@MSI>
2023-11-29 10:47:16 -0500

    Atualizacion de crear pacientes, controlador de pacientes

--- src/clases/Paciente.java
+++ src/clases/Paciente.java
@@ -8,14 +8,20 @@ import java.time.LocalDate;
  */
 public class Paciente extends Persona {
     String tipoSagre;
-    Boolean genero;
+    String genero;
     String antededentes;
-    float altura;
-    float peso;
+    String altura;
+    String peso;
     
     
-    public Paciente(String nombre, String apellido, String cedula, LocalDate fechaNaci) {
+    public Paciente(String nombre, String apellido, String cedula, LocalDate fechaNaci, String tipo, String gene,String altura,
+            String peso, String antecedentes) {
         super(nombre, apellido, cedula, fechaNaci);
+        this.tipoSagre = tipo;
+        this.genero = gene;
+        this.altura = altura;
+        this.peso = peso;
+        this.antededentes = antededentes;
         
     }
     
