ddb2f1a9d7de675af8c655fb58520bcb5685a0c1
usuario <usuario@DESKTOP-4K77I3C>
2024-01-04 19:02:03 -0500

    menu principal y control botones

--- src/controladores/ControladorLogin.java
+++ src/controladores/ControladorLogin.java
@@ -2,6 +2,7 @@ package controladores;
 
 import base.MetodosSQL;
 import clases.Usuario;
+import interfaces.FrmMenP;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -19,13 +20,25 @@ public class ControladorLogin {
         try {
             if (new Encriptacion().desencriptar(tempUser.contraseña).equals(contraseña)) {
                 JOptionPane.showMessageDialog(null, "Inicio de sesión exitoso.");
+                this.ocultarBoton(tempUser);
                 return true;
             }
         } catch (Exception ex) {
             Logger.getLogger(ControladorLogin.class.getName()).log(Level.SEVERE, null, ex);
         }
-        JOptionPane.showMessageDialog(null,"La contraseña es incorrecta");
+        JOptionPane.showMessageDialog(null, "La contraseña es incorrecta");
         return false;
     }
 
+    public void ocultarBoton(Usuario tempUser) {
+        FrmMenP menP = new FrmMenP();
+        menP.setVisible(true);
+        menP.btn_pacientes.setEnabled(true);
+        menP.btn_usuarios.setEnabled(tempUser.rol == 0 || tempUser.rol == 1);
+        menP.btn_citas.setEnabled(tempUser.rol == 0 || tempUser.rol == 1 || tempUser.rol == 2);
+        menP.btn_consulta.setEnabled(tempUser.rol == 0 || tempUser.rol == 1 || tempUser.rol == 2 || tempUser.rol == 3);
+        menP.btn_farmacia.setEnabled(tempUser.rol == 0 || tempUser.rol == 1 || tempUser.rol == 2);
+        menP.btn_reportes.setEnabled(tempUser.rol == 0 || tempUser.rol == 1 || tempUser.rol == 3);
+
+    }
 }
